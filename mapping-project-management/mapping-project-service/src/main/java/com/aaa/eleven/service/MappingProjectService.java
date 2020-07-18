1ecc936a6b5514543debe1f15eaca643ec784f0e
ningyan-git <67271972+ningyan-git@users.noreply.github.com>
2020-07-18 11:49:42 +0800

    2020年7月18日11:46:34      项目汇交

--- mapping-project-management/mapping-project-service/src/main/java/com/aaa/eleven/service/MappingProjectService.java
+++ mapping-project-management/mapping-project-service/src/main/java/com/aaa/eleven/service/MappingProjectService.java
@@ -3,12 +3,19 @@ package com.aaa.eleven.service;
 import com.aaa.eleven.base.BaseService;
 import com.aaa.eleven.mapper.MappingProjectMapper;
 import com.aaa.eleven.model.MappingProject;
+import com.aaa.eleven.utils.FileNameUtils;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.apache.http.client.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static com.aaa.eleven.staticproperties.TimeForatProperties.TIME_FORMAT;
 
 /**
  * @Company
@@ -37,4 +44,96 @@ public class MappingProjectService extends BaseService<MappingProject> {
         }
         return null;
     }
+    /***
+     * @Author ftt
+     * @Description
+     * 根据userid查询项目 + 项目类型
+     * @Date 2020/7/18 10:11
+     * @Param [map]
+     * @return com.github.pagehelper.PageInfo<com.aaa.eleven.model.MappingProject>
+     */
+    public PageInfo<MappingProject> selectByUserId(Map map) {
+        //获取分页信息
+        Integer pageNum = Integer.parseInt(map.get("pageNum").toString());
+        Integer pageSize = Integer.parseInt(map.get("pageSize").toString());
+        PageHelper.startPage(pageNum,pageSize);
+        //获取查询条件
+        MappingProject project = new MappingProject();
+        Long userId = Long.parseLong(map.get("userId").toString());
+        String projectType = "";
+        if(map.get("projectType") != null ){
+            projectType =map.get("projectType").toString();
+        }
+        if (projectType == null && "".equals(projectType)) {
+            project.setUserId(userId);
+        }else {
+            project.setUserId(userId).setProjectType(projectType);
+        }
+        //进行查询
+        List<MappingProject> projects = mappingProjectMapper.select(project);
+        //判断查询结果是否为空
+        if (projects.size() > 0) {
+            return new PageInfo(projects);
+        }
+        return null;
+    }
+    /***
+     * @Author ftt
+     * @Description
+     * 新增项目
+     * @Date 2020/7/18 10:24
+     * @Param [mappingProject]
+     * @return java.lang.Boolean
+     */
+    public Boolean insertMappingProject(MappingProject mappingProject){
+        if(mappingProject != null){
+           mappingProject.setId(Long.parseLong(FileNameUtils.getFileName()));
+           mappingProject.setCreateTime(DateUtils.formatDate(new Date(),TIME_FORMAT));
+            Integer flag = insert(mappingProject);
+            if(flag > 0){
+                return true;
+            }
+        }
+        return false;
+    }
+    /***
+     * @Author ftt
+     * @Description
+     * 修改项目
+     * @Date 2020/7/18 10:24
+     * @Param [mappingProject]
+     * @return java.lang.Boolean
+     */
+    public Boolean updateMappingProject(MappingProject mappingProject){
+        if(mappingProject != null){
+            if(mappingProject.getId() != null){
+                mappingProject.setModifyTime(DateUtils.formatDate(new Date(),TIME_FORMAT));
+                Integer flag = update(mappingProject);
+                if(flag > 0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    /***
+     * @Author ftt
+     * @Description
+     * 删除项目
+     * @Date 2020/7/18 10:24
+     * @Param [mappingProject]
+     * @return java.lang.Boolean
+     */
+    public Boolean deleteMappingProject(MappingProject mappingProject){
+        if(mappingProject != null){
+            if(mappingProject.getId() != null){
+                Integer flag = delete(mappingProject);
+                if(flag > 0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
--- mapping-project-provider-management/mapping-project-provider/src/main/java/com/aaa/eleven/controller/MappingProjectController.java
+++ mapping-project-provider-management/mapping-project-provider/src/main/java/com/aaa/eleven/controller/MappingProjectController.java
@@ -7,9 +7,9 @@ import com.aaa.eleven.model.MappingProject;
 import com.aaa.eleven.service.MappingProjectService;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @Company
@@ -39,6 +39,74 @@ public class MappingProjectController extends CommonController<MappingProject> {
             return selectSuccess(pageInfo);
         }else {
             return selectFailed();
+        }
+    }
+    /***
+     * @Author ftt
+     * @Description
+     * 根据userid查询项目 + 项目类型
+     * @Date 2020/7/18 10:20
+     * @Param [map]
+     * @return com.aaa.eleven.base.ResultData
+     */
+    @GetMapping("/mappingProject/selectByUserId")
+    public ResultData selectByUserId(@RequestParam Map map){
+        PageInfo<MappingProject> pageInfo = mappingProjectService.selectByUserId(map);
+        if(null != pageInfo){
+            return selectSuccess(pageInfo);
+        }else {
+            return selectFailed();
+        }
+    }
+    /***
+     * @Author ftt
+     * @Description
+     * 新增mappingProject
+     * @Date 2020/7/18 10:31
+     * @Param [mappingProject]
+     * @return com.aaa.eleven.base.ResultData
+     */
+    @PostMapping("/mappingProject/insertMappingProject")
+    public ResultData insertMappingProject(@RequestBody MappingProject mappingProject){
+        Boolean flag = mappingProjectService.insertMappingProject(mappingProject);
+        if(flag){
+            return insertSuccess();
+        }else {
+            return insertFailed();
+        }
+    }
+    /***
+     * @Author ftt
+     * @Description
+     * 修改mappingProject
+     * @Date 2020/7/18 10:31
+     * @Param [mappingProject]
+     * @return com.aaa.eleven.base.ResultData
+     */
+    @PostMapping("/mappingProject/updateMappingProject")
+    public ResultData updateMappingProject(@RequestBody MappingProject mappingProject){
+        Boolean flag = mappingProjectService.updateMappingProject(mappingProject);
+        if(flag){
+            return updateSuccess();
+        }else {
+            return updateFailed();
+        }
+    }
+    /***
+     * @Author ftt
+     * @Description
+     * 删除mappingProject
+     * @Date 2020/7/18 10:31
+     * @Param [mappingProject]
+     * @return com.aaa.eleven.base.ResultData
+     */
+    @PostMapping("/mappingProject/deleteMappingProject")
+    public ResultData deleteMappingProject(@RequestBody MappingProject mappingProject){
+        Boolean flag = mappingProjectService.deleteMappingProject(mappingProject);
+        if(flag){
+            return deleteSuccess();
+        }else {
+            return deleteFailed();
         }
     }
 }
